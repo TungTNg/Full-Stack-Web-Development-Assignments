90b6edbebbcb674de3f7c1471846be5cb57b86c4
Tung Nguyen <39467840+TungTNg@users.noreply.github.com>
2018-06-28 14:35:40 -0700

    Added Stalker Exercise (Javascript exercise)

---
+++ JS Exercises/stalkerExcersise.js
@@ -0,0 +1,7 @@
+var firstName = prompt("What is your first name?"),
+ 	lastName = prompt("What is your last name?"),
+ 	userAge = prompt("What is your age?");
+
+alert('Your full name is: ' + firstName + ' ' + lastName + '\nYou\'re ' + userAge + ' years old!');
+
+console.log('Your full name is: ' + firstName + ' ' + lastName + '\nYou\'re ' + userAge + ' years old!');
