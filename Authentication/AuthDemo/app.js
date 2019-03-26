f748715901c5b3920cdc1bca869d56e37aa470d5
Tung Nguyen <mecer80@gmail.com>
2019-03-26 05:36:02 +0000

    Added Secret Page Code Along Pt.2

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -1,13 +1,30 @@
-var express             = require('express'),
-    app                 = express(),
-    bodyParser          = require('body-parser'),
-    mongoose            = require('mongoose');
+var express                 = require('express'),
+    app                     = express(),
+    passport                = require("passport"),
+    bodyParser              = require('body-parser'),
+    User                    = require("./models/user"),
+    LocalStrategy           = require("passport-local"),
+    passportLocalMongoose   = require("passport-local-mongoose"),
+    mongoose                = require('mongoose');
 
 // APP CONFIG
 mongoose.connect("mongodb://localhost:27017/auth_demo_app", { useNewUrlParser: true });
 app.set("view engine", "ejs");
+
+app.use(require("express-session")({
+    secret: "This is my secret key",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.set("useFindAndModify", false);
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res) {
     res.render("home");
