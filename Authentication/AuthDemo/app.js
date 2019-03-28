b9e813c883c66c8722900e27d05e12ddeb2f17a0
Tung Nguyen <mecer80@gmail.com>
2019-03-28 04:54:36 +0000

    Secret Page Code Alnog Pt. 5

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -35,7 +35,7 @@ app.get("/", function(req, res) {
     res.render("home");
 });
 
-app.get("/secret", function(req, res) {
+app.get("/secret", isLoggedIn, function(req, res) {
     res.render("secret");
 });
 
@@ -72,6 +72,18 @@ app.post("/login", passport.authenticate("local", {
     failureRedirect: "/app/login"
 }), function(req, res) {
 });
+
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/app");
+});
+
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/app/login");
+}
 
 // RUN CONFIG
 app.listen(3080, function() {
